3f05ae10f1cb064b1db0019a754b632ab858d21e
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-05-07 14:12:50 +0200

    issue #54: still WIP, tried to first build with yarn, then with tauri

--- frontend/flake.nix
+++ frontend/flake.nix
@@ -15,35 +15,33 @@
     in {
       packages = forAllSystems (system: let pkgs = pkgs-s.${system}; in rec {
 
-        default = with pkgs; mkYarnPackage {
+        frontend = with pkgs; mkYarnPackage {
           name = pname;
           version = "0.5.0";
           src = ./.;
-          nativeBuildInputs = [ cargo-tauri cargoDeps nodePackages.pnpm cargo nodejs jq];
-          yarnFlags = ["--frozen-lockfile"];
+          nativeBuildInputs = [ 
+            cargo-tauri 
+            # cargoDeps 
+            nodePackages.pnpm 
+            cargo 
+            nodejs 
+            jq
+          ];
+          buildInputs = [ 
+            # cargoDeps
+          ];
+          # yarnFlags = ["--frozen-lockfile"];
           
-          # buildInputs = [ gtk3 wayland xorg.libxcb.dev ];
           # preBuild = ''
           #   export HOME=$(mktemp -d)
-          #   pnpm config set store-dir ${pnpmDeps}
-          #   chmod +w ..
-          #   pnpm install --offline --frozen-lockfile --no-optional --ignore-script
-          #   ls -la node_modules/.pnpm/@tauri-apps+cli@1.5.7/node_modules/@tauri-apps/cli/
-          #   # pnpm rebuild
-          #   echo "pnpm install done, building cargo"
-          #   # Use cargo-tauri from nixpkgs instead of pnpm tauri from npm
-          #   cargo tauri build -v
+          #   mkdir -p deps
+          #   mkdir -p deps/core_lib
+          #   cp -r ${inputs.core.packages.${system}.rust-bin.src}/* deps/core_lib
           # '';
 
           # preInstall = ''
           #   ls
           # '';
-
-          # pkgConfig = {
-          #   core_lib = {
-              
-          #   };
-          # };
 
           # preBuild = ''
           #   export HOME=$(mktemp -d)
@@ -59,7 +57,6 @@
           # packageJSON = patchedPackageJSON ;#src + "/nix-modded-package.json";
           packageJSON = pkgs.runCommand "${pname}-package.json" { } ''
             ${jq}/bin/jq 'del(.dependencies["@martichou/core_lib"])' ${self}/package.json > $out
-            cat $out
           '';
 
           # workspaceDependencies = [
@@ -87,7 +84,13 @@
           # '';
 
           # buildPhase = ''
+          #   runHook preBuild
+
           #   # yarn --offline build --verbose
+          #   ls -la deps/core_lib
+          #   cargo tauri build --verbose
+
+          #   runHook postBuild
           # '';
 
           # offlineCache = symlinkJoin { 
@@ -99,26 +102,15 @@
           #     (inputs.core.packages.${system}.offline-cache)
           #   ];
           # };
-
           
-          meta = with lib; {
-            description = "frontend for rquickshare";
-            homepage = "https://github.com/Martichou/rquickshare/tree/master/frontend";
-            license = licenses.gpl3;
-            platforms = lib.platforms.linux; #FIXME darwin #22
-            maintainers = with maintainers; [ hannesgith ];
-          };
-          # ESBUILD_BINARY_PATH = "${lib.getExe (esbuild.override {
-          #   buildGoModule = args: buildGoModule (args // rec {
-          #     version = "0.19.8";
-          #     src = fetchFromGitHub {
-          #       owner = "evanw";
-          #       repo = "esbuild";
-          #       rev = "v${version}";
-          #       hash = "sha256-f13YbgHFQk71g7twwQ2nSOGA0RG0YYM01opv6txRMuw=";
-          #     };
-          #   });
-          # })}";
+          # meta = with lib; {
+          #   description = "frontend for rquickshare";
+          #   homepage = "https://github.com/Martichou/rquickshare/tree/master/frontend";
+          #   license = licenses.gpl3;
+          #   platforms = lib.platforms.linux; #FIXME darwin #22
+          #   maintainers = with maintainers; [ hannesgith ];
+          # };
+          
         };
 
         # pnpmDeps = with pkgs; stdenvNoCC.mkDerivation {
@@ -147,13 +139,79 @@
 
         # yarnDeps = with pkgs; 
 
-        cargoDeps = with pkgs; rustPlatform.importCargoLock {
-          lockFile = ./src-tauri/Cargo.lock;
-          outputHashes = {
-            "mdns-sd-0.10.4" = "sha256-y8pHtG7JCJvmWCDlWuJWJDbCGOheD4PN+WmOxnakbE4=";
-            "tauri-plugin-autostart-0.0.0" = "sha256-uOPFpWz715jT8zl9E6cF+tIsthqv4x9qx/z3dJKVtbw=";
+        cargoDeps = with pkgs; symlinkJoin {
+          name = "${pname}-cargoDeps-joined";
+          paths = [
+            (runCommand "${pname}-rqs_lib-src" { } ''
+              mkdir -p $out
+              mkdir -p $out/rqs_lib
+              mkdir -p $out/core_lib
+              cp -r ${inputs.core.packages.${system}.rust-bin.src }/* $out/rqs_lib
+              cp -r ${inputs.core.packages.${system}.rust-bin.src }/* $out/core_lib
+            '')
+            (rustPlatform.importCargoLock {
+              lockFile = runCommand "${pname}-srcTauri-cargo.lock" { } ''
+                sed '/name = "rqs_lib"/,/^$/d' ${self}/src-tauri/Cargo.lock > $out
+                sed -i '/rqs_lib/d' $out
+              '';
+              # lockFile = ./src-tauri/Cargo.lock;
+              outputHashes = {
+                "mdns-sd-0.10.4" = "sha256-y8pHtG7JCJvmWCDlWuJWJDbCGOheD4PN+WmOxnakbE4=";
+                "tauri-plugin-autostart-0.0.0" = "sha256-uOPFpWz715jT8zl9E6cF+tIsthqv4x9qx/z3dJKVtbw=";
+              };
+            })
+          ];
+        };
+
+        default = with pkgs; rustPlatform.buildRustPackage {
+          inherit pname;
+          name = "${pname}-actual";
+          src = ./.;
+          buildInputs = [ ];
+          nativeBuildInputs = [ pkg-config ];
+
+          sourceRoot = "source/src-tauri";
+
+          cargoLock = {
+            lockFile = runCommand "${pname}-srcTauri-cargo.lock" { } ''
+                sed '/name = "rqs_lib"/,/^$/d' ${self}/src-tauri/Cargo.lock > $out
+                sed -i '/rqs_lib/d' $out
+              '';
+            outputHashes = {
+                "mdns-sd-0.10.4" = "sha256-y8pHtG7JCJvmWCDlWuJWJDbCGOheD4PN+WmOxnakbE4=";
+                "tauri-plugin-autostart-0.0.0" = "sha256-uOPFpWz715jT8zl9E6cF+tIsthqv4x9qx/z3dJKVtbw=";
+              };
+          };
+
+          # copy the frontend static resources to final build directory
+          # Also modify tauri.conf.json so that it expects the resources at the new location
+            # cp ${./Cargo.lock} Cargo.lock
+          postPatch = ''
+
+            mkdir -p frontend-build
+            cp -R ${frontend}/src frontend-build
+
+            substituteInPlace tauri.conf.json --replace '"distDir": "../dist",' '"distDir": "frontend-build/src",'
+          '';
+
+          checkFlags = [
+            # tries to mutate the parent directory
+            "--skip=test_file_operation"
+          ];
+
+          postInstall = ''
+            mv $out/bin/app $out/bin/rquickshare
+          '';
+
+          meta = with lib; {
+            description = "frontend for rquickshare";
+            homepage = "https://github.com/Martichou/rquickshare/tree/master/frontend";
+            license = licenses.gpl3;
+            platforms = lib.platforms.linux; #FIXME darwin #22
+            maintainers = with maintainers; [ hannesgith ];
           };
         };
+
       });
     };
 }
