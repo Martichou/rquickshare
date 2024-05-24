1df0a14340fdab2c1628a7c47be28ba615ca5c22
Martichou <m@rtin.fyi>
2024-05-24 23:11:58 +0200

    feat: add logger
    Signed-off-by: Martichou <m@rtin.fyi>

--- app/nogui/src/main.rs
+++ app/nogui/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+use logger::set_up_logging;
+use rqs_lib::RQS;
+
+mod logger;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    set_up_logging()?;
+
+    // Start the RQuickShare service
+    let mut rqs = RQS::default();
+    rqs.run().await?;
+
+    // Still need to add everything here
+    todo!();
+
+    info!("Stopping service.");
+    rqs.stop().await;
+
+    Ok(())
 }
