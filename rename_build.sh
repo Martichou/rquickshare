c851df39eca82d45feca05363d3712d163e51f6d
Martichou <m@rtin.fyi>
2024-07-04 19:48:13 +0200

    ci: release with macos :)
    Signed-off-by: Martichou <m@rtin.fyi>

--- rename_build.sh
+++ rename_build.sh
@@ -10,10 +10,15 @@ base_dir="$1"
 tauri_ver="$2"
 
 # Find all relevant files in the base directory
-files=$(find "$base_dir" -type f \( -name "*.deb" -o -name "*.rpm" -o -name "*.AppImage" \))
+files=$(find "$base_dir" -type f \( -name "*.deb" -o -name "*.rpm" -o -name "*.AppImage" -o -name "*.dmg" \))
 
-glib_ver=$(ldd --version | head -n1 | awk '{print $NF}')
-echo "GLIBC version: ${glib_ver}"
+
+if command -v ldd &> /dev/null; then
+    glib_ver=$(ldd --version | head -n1 | awk '{print $NF}')
+    echo "GLIBC version: ${glib_ver}"
+else
+    echo "ldd command not found."
+fi
 
 # Loop through each file
 for file in $files; do
@@ -38,7 +43,11 @@ for file in $files; do
     fi
 
     # Construct the new filename
-    new_filename="r-quick-share_${version}_glibc-${glib_ver}_${anything}.${extension}"
+    if [ -n "$glib_ver" ]; then
+        new_filename="r-quick-share_${version}_glibc-${glib_ver}_${anything}.${extension}"
+    else
+        new_filename="r-quick-share_${version}_${anything}.${extension}"
+    fi
 
     # Rename the file
     mv "$file" "$dir/$new_filename"
