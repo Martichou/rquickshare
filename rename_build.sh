ae4e17879cb42f80ab457ddb79e9ef7efa0cd7d5
Martichou <m@rtin.fyi>
2024-08-04 20:12:42 +0200

    ci: publish all bundle & ensure no cached one
    Signed-off-by: Martichou <m@rtin.fyi>

--- rename_build.sh
+++ rename_build.sh
@@ -2,12 +2,13 @@
 
 # Check if the base directory argument is provided
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <base_directory> <build_tauri_ver>"
+  echo "Usage: $0 <base_directory> <build_tauri_ver> [debug_mode]"
   exit 1
 fi
 
 base_dir="$1"
 tauri_ver="$2"
+debug_mode="$3"
 
 # Find all relevant files in the base directory
 files=$(find "$base_dir" -type f \( -name "*.deb" -o -name "*.rpm" -o -name "*.AppImage" -o -name "*.dmg" \))
@@ -42,11 +43,19 @@ for file in $files; do
         continue
     fi
 
-    # Construct the new filename
+    # Construct the new filename based on the presence of glibc and debug_mode
     if [ -n "$glib_ver" ]; then
-        new_filename="r-quick-share-${tauri_ver}_${version}_glibc-${glib_ver}_${anything}.${extension}"
+        if [ -n "$debug_mode" ]; then
+            new_filename="r-quick-share-${tauri_ver}-debug_${version}_glibc-${glib_ver}_${anything}.${extension}"
+        else
+            new_filename="r-quick-share-${tauri_ver}_${version}_glibc-${glib_ver}_${anything}.${extension}"
+        fi
     else
-        new_filename="r-quick-share-${tauri_ver}_${version}_${anything}.${extension}"
+        if [ -n "$debug_mode" ]; then
+            new_filename="r-quick-share-${tauri_ver}-debug_${version}_${anything}.${extension}"
+        else
+            new_filename="r-quick-share-${tauri_ver}_${version}_${anything}.${extension}"
+        fi
     fi
 
     # Rename the file
