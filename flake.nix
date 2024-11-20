7e7185354f7d5cfb7d4d76d8c3ea73c890585646
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-11-20 23:42:47 +0100

    flake: more in line with downstream

--- flake.nix
+++ flake.nix
@@ -3,65 +3,116 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    corelib.url = "./core_lib";
   };
 
-  outputs = inputs@{ nixpkgs, ... }:
+  outputs =
+    inputs@{ nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
       # definitely not tested, but i like it exposed, who knows
       systems = lib.systems.flakeExposed;
       forAllSystems = lib.genAttrs systems;
-      spkgs = system : nixpkgs.legacyPackages.${system}.pkgs;
+      spkgs = system: nixpkgs.legacyPackages.${system}.pkgs;
     in
     {
-      packages = forAllSystems (system: with spkgs system; rec {
-        tauri = cargo-tauri;
-        corelib_pkgs = inputs.corelib.outputs.packages.${system};
-        corelib_src = corelib_pkgs.rqscore_src;
-        corelib_path = "rqs_lib";
-        rquickshare = rustPlatform.buildRustPackage rec {
-          name = "rquickshare";
-          src = ./app/main;
-          nativeBuildInputs = [ tauri.hook pnpm.configHook nodejs moreutils protobuf ];
-          cargoRoot = "src-tauri";
-          cargoLock = {
-            lockFile = app/main/src-tauri/Cargo.lock;
-            outputHashes = {
-              "mdns-sd-0.10.4" = "sha256-y8pHtG7JCJvmWCDlWuJWJDbCGOheD4PN+WmOxnakbE4=";
+      packages = forAllSystems (
+        system: with spkgs system; rec {
+          tauri = cargo-tauri;
+          # we could use rustPlatform.buildRustPackage, but there cargoDeps doesn't work
+          rquickshare = let
+            pname = "rquickshare";
+            src = let fs = lib.fileset; in fs.toSource {
+              root = ./.;
+              fileset = fs.difference ./. (
+                fs.unions [
+                  ./flake.nix
+                  ./flake.lock
+                ]
+              );
+            };
+          in
+          stdenv.mkDerivation rec {
+            inherit src pname;
+            name = pname;
+            sourceRoot = "${src.name}/app/main";
+            corelibSourceRoot = "${src.name}/core_lib";
+            cargoRoot = "src-tauri";
+            corelibPath = "rqs_lib";
+            nativeBuildInputs = [
+              tauri.hook
+              pnpm.configHook
+              nodejs
+              moreutils
+              protobuf
+              jq
+              yq
+              rustPlatform.cargoSetupHook
+            ];
+            # seemingly the fetcher ignores cargoRoot (contrary to the docs)
+            cargoDeps = rustPlatform.fetchCargoVendor {
+              inherit src pname;
+              hash = cargoHash;
+              sourceRoot = "${sourceRoot}/${cargoRoot}";
+            };
+            cargoHash = "sha256-m+BHN0eCF8rwCBWQ94XWHP7vmtQMtT/9adFgcU0BDTQ=";
+            pnpmDeps = pnpm.fetchDeps {
+              inherit src sourceRoot pname;
+              hash = "sha256-ko0S934TnGDVbTrFkunHPt0e/pT5CVi4yorTi0WYqmc=";
+            };
+            postUnpack =
+              let
+                fullCorePath = "${sourceRoot}/${cargoRoot}/${corelibPath}";
+              in
+              ''
+                cp -r ${corelibSourceRoot} ${fullCorePath} && chmod -R +w ${fullCorePath}
+              '';
+            # remove macOS signing and relative links
+            postConfigure = ''
+              jq     'del(.bundle.macOS.signingIdentity, .bundle.macOS.hardenedRuntime)' src-tauri/tauri.conf.json | sponge src-tauri/tauri.conf.json
+              yq -iy '.importers.".".dependencies."@martichou/core_lib".specifier = "link:${corelibPath}" | .importers.".".dependencies."@martichou/core_lib".version = "link:${corelibPath}"' pnpm-lock.yaml
+              jq     '.dependencies."@martichou/core_lib" = "link:${corelibPath}"' package.json | sponge package.json
+              sed -i 's|path = "../../../core_lib"|path = "${corelibPath}"|' ${cargoRoot}/Cargo.toml
+            '';
+            checkPhase = "true"; # idk why checks fail, todo
+            installPhase =
+              let
+                path = "${cargoRoot}/target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/bundle";
+              in
+              if stdenv.isDarwin then
+                ''
+                  mkdir -p $out/bin
+                  mv ${path}/macos $out/Applications
+                  echo "#!${lib.getExe zsh}" >> $out/bin/${name}
+                  echo "open -a $out/Applications/${name}.app" >> $out/bin/${name}
+                  chmod +x $out/bin/${name}
+                ''
+              else
+                ''
+                  mv ${path}/deb/*/data/usr $out
+                '';
+            meta = {
+              description = "Rust implementation of NearbyShare/QuickShare from Android for Linux";
+              homepage = "https://github.com/Martichou/rquickshare";
+              changelog = "https://github.com/Martichou/rquickshare/blob/master/CHANGELOG.md";
+              license = lib.licenses.gpl3Plus;
+              maintainers = with lib.maintainers; [ hannesgith ];
+              platforms = with lib.platforms; linux ++ darwin;
+              mainProgram = "rquickshare";
             };
           };
-          pnpmDeps = pnpm.fetchDeps {
-            inherit src;
-            pname = name;
-            hash = "sha256-ko0S934TnGDVbTrFkunHPt0e/pT5CVi4yorTi0WYqmc=";
+          default = rquickshare;
+        }
+      );
+      devShells = forAllSystems (
+        system: with spkgs system; {
+          default = mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              nodejs
+            ];
           };
-          # remove macOS signing and relative links
-          postConfigure = ''
-            ${jq}/bin/jq     'del(.bundle.macOS.signingIdentity, .bundle.macOS.hardenedRuntime)' src-tauri/tauri.conf.json | sponge src-tauri/tauri.conf.json
-            ${yq}/bin/yq -iy '.importers.".".dependencies."@martichou/core_lib".specifier = "link:${corelib_path}" | .importers.".".dependencies."@martichou/core_lib".version = "link:${corelib_path}"' pnpm-lock.yaml
-            ${jq}/bin/jq     '.dependencies."@martichou/core_lib" = "link:${corelib_path}"' package.json | sponge package.json
-            sed -i 's|path = "../../../core_lib"|path = "${corelib_path}"|' ${cargoRoot}/Cargo.toml
-            cp -r ${corelib_src} ${cargoRoot}/${corelib_path} && chmod -R +w ${cargoRoot}/${corelib_path}
-          '';
-          checkPhase = "true"; # idk why checks fail, todo
-          installPhase = let path = "${cargoRoot}/target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/bundle"; in 
-          if stdenv.isDarwin then ''
-            mkdir -p $out/bin
-            mv ${path}/macos $out/Applications
-            echo "#!${zsh}/bin/zsh" >> $out/bin/${name}
-            echo "open -a $out/Applications/${name}.app" >> $out/bin/${name}
-            chmod +x $out/bin/${name}
-          '' else ''
-            mv ${path}/deb/*/data/usr $out
-          '';
-        };
-        default = rquickshare;
-      });
-      devShells = forAllSystems (system: with spkgs system; {
-        default = mkShell {
-          buildInputs = [ cargo rustc nodejs ];
-        };
-      });
+        }
+      );
     };
 }
