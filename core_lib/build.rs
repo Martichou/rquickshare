b8cce9c9610fc9d637f5b528013ac467eb4ed914
nozwock <57829219+nozwock@users.noreply.github.com>
2025-05-03 14:55:16 +0530

    Move TS bind-gen in build.rs under 'ts-support' feature flag

--- core_lib/build.rs
+++ core_lib/build.rs
@@ -18,21 +18,24 @@ fn main() {
     )
     .unwrap();
 
-    let mut exports: Vec<_> = fs::read_dir("./bindings")
-        .unwrap()
-        .filter_map(Result::ok)
-        .filter_map(|p| {
-            p.path()
-                .file_stem()
-                .and_then(OsStr::to_str)
-                .map(str::to_owned)
-        })
-        .filter(|f| f != "index")
-        .map(|f| format!("export * from \"./{}\"", f))
-        .collect();
-    // Sort it to avoid having the index.ts being different for no reason
-    exports.sort();
+    #[cfg(feature = "ts-support")]
+    {
+        let mut exports: Vec<_> = fs::read_dir("./bindings")
+            .unwrap()
+            .filter_map(Result::ok)
+            .filter_map(|p| {
+                p.path()
+                    .file_stem()
+                    .and_then(OsStr::to_str)
+                    .map(str::to_owned)
+            })
+            .filter(|f| f != "index")
+            .map(|f| format!("export * from \"./{}\"", f))
+            .collect();
+        // Sort it to avoid having the index.ts being different for no reason
+        exports.sort();
 
-    let mut file = File::create("./bindings/index.ts").unwrap();
-    file.write_all(exports.join("\n").as_bytes()).unwrap();
+        let mut file = File::create("./bindings/index.ts").unwrap();
+        file.write_all(exports.join("\n").as_bytes()).unwrap();
+    }
 }
