99b70bc257c8fc49df7e44ec893e14cf90afaa95
Martichou <m@rtin.fyi>
2025-06-16 17:46:06 +0200

    feat!: remove legacy variant
    Due to Github removing support for the Ubuntu-20.04 os image,
    I'm removing the legacy variant that was kept alive only for
    backward compatibility. RQuickShare will sadly align with Tauri
    v2 system requirements.
    
    The previous release v0.11.5 will be the last one to include
    the legacy binary. This version is still working and will be the
    one people wanting to use RQuickShare on "older" system shall
    default to.
    
    Signed-off-by: Martichou <m@rtin.fyi>

--- app/legacy/src-tauri/src/cmds/get_hostname.rs
+++
@@ -1,4 +0,0 @@
-#[tauri::command]
-pub fn get_hostname() -> String {
-    sys_metrics::host::get_hostname().unwrap_or(String::from("Unknown"))
-}
--- fmt.sh
+++ fmt.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-for dir in core_lib app/legacy/src-tauri app/main/src-tauri; do
+for dir in core_lib app/main/src-tauri; do
   find "$dir" -name '*.rs' -not -path "*/target/*" -exec rustfmt {} +
 done
-
-# Check if there are changes in the /app/legacy/src directory
-if git diff --name-only HEAD | grep '^app/legacy/src/' >/dev/null; then
-    echo "Changes detected in /app/legacy/src. Executing script..."
-
-    # Navigate to the app/legacy directory
-    cd app/legacy
-
-    # Run your commands
-    pnpm lint --fix
-
-    cd ../..
-else
-    echo "No changes detected in /app/legacy/src."
-fi
 
 # Check if there are changes in the /app/main/src directory
 if git diff --name-only HEAD | grep '^app/main/src/' >/dev/null; then
